04cf909b93b22a7dec5850c9cff9647c2cc38cc2
KatrielKoga <katriel.koga.2012@hotmail.com>
2025-11-01 07:08:28 -0300

    feat: add `features` column to `users` table

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -119,6 +119,7 @@ describe("PATCH /api/v1/users/[username]", () => {
         username: "uniqueUser2",
         email: createdUser.email,
         password: responseBody.password,
+        features: [],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -154,6 +155,7 @@ describe("PATCH /api/v1/users/[username]", () => {
         username: createdUser.username,
         email: "uniqueEmail2@curso.dev",
         password: responseBody.password,
+        features: [],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -189,6 +191,7 @@ describe("PATCH /api/v1/users/[username]", () => {
         username: createdUser.username,
         email: createdUser.email,
         password: responseBody.password,
+        features: [],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
