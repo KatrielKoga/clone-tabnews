4f441cb4275123b2a42567a98a965d18ff06fb53
Katriel Koga <katriel.koga.2012@hotmail.com>
2025-12-06 13:27:15 -0300

    feat: require `read:activation_token` to access `/api/v1/activations/[token_id]`

--- models/activation.js
+++ models/activation.js
@@ -1,8 +1,9 @@
 import email from "infra/email";
 import database from "infra/database.js";
 import webserver from "infra/webserver.js";
-import { UnauthorizedError } from "infra/errors.js";
+import { ForbiddenError, NotFoundError } from "infra/errors.js";
 import user from "./user.js";
+import authorization from "./authorization.js";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // 15 minutes
 
@@ -53,7 +54,7 @@ async function findOneValidById(userActivationTokenId) {
     });
 
     if (results.rowCount === 0) {
-      throw new UnauthorizedError({
+      throw new NotFoundError({
         message:
           "O token de ativação utilizado não foi encontrado no sistema ou expirou.",
         action: "Faça um novo cadastro.",
@@ -103,6 +104,15 @@ async function markTokenAsUsed(activationTokenId) {
 }
 
 async function activateUserByUserId(userId) {
+  const userToActivate = await user.findOneById(userId);
+
+  if (!authorization.can(userToActivate, "read:activation_token")) {
+    throw new ForbiddenError({
+      message: "Você não pode mais utilizar tokens de ativação.",
+      action: "Entre em contato com o suporte.",
+    });
+  }
+
   const activatedUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
@@ -116,6 +126,7 @@ const activation = {
   findOneValidById,
   markTokenAsUsed,
   activateUserByUserId,
+  EXPIRATION_IN_MILLISECONDS,
 };
 
 export default activation;
