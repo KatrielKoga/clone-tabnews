00b38deea950643c8bf0d07cecc5e264702a6a60
Katriel Koga <katriel.koga.2012@hotmail.com>
2025-12-06 11:52:16 -0300

    feat: require `read:session` to access `/user` endpoint

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -11,7 +11,7 @@ beforeAll(async () => {
 });
 
 describe("Use case: Registration Flow (all successful)", () => {
-  let createUserResponseBody, activationTokenId;
+  let createUserResponseBody, activationTokenId, createSessionResponseBody;
   test("Create user account", async () => {
     const createUserResponse = await fetch(
       "http://localhost:3000/api/v1/users",
@@ -78,7 +78,7 @@ describe("Use case: Registration Flow (all successful)", () => {
     expect(Date.parse(activationResponseBody.used_at)).not.toBeNaN();
 
     const activatedUser = await user.findOneByUsername("RegistrationFlow");
-    expect(activatedUser.features).toEqual(["create:session"]);
+    expect(activatedUser.features).toEqual(["create:session", "read:session"]);
   });
   test("Login", async () => {
     const createSessionResponse = await fetch(
@@ -97,9 +97,21 @@ describe("Use case: Registration Flow (all successful)", () => {
 
     expect(createSessionResponse.status).toBe(201);
 
-    const createSessionREsponseBody = await createSessionResponse.json();
+    createSessionResponseBody = await createSessionResponse.json();
 
-    expect(createSessionREsponseBody.user_id).toBe(createUserResponseBody.id);
+    expect(createSessionResponseBody.user_id).toBe(createUserResponseBody.id);
   });
-  test("Get user information", async () => {});
+  test("Get user information", async () => {
+    const userResponse = await fetch("http://localhost:3000/api/v1/user", {
+      headers: {
+        cookie: `session_id=${createSessionResponseBody.token}`,
+      },
+    });
+
+    expect(userResponse.status).toBe(200);
+
+    const userResponseBody = await userResponse.json();
+
+    expect(userResponseBody.id).toBe(createUserResponseBody.id);
+  });
 });
