ec338de2d8c04e0f137d287cb802755247b1b672
Katriel Koga <katriel.koga.2012@hotmail.com>
2025-12-06 10:46:23 -0300

    feat: add `injectAnonymousOrUser` and `canRequest` middlewares to `/sessions`

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -80,6 +80,26 @@ describe("Use case: Registration Flow (all successful)", () => {
     const activatedUser = await user.findOneByUsername("RegistrationFlow");
     expect(activatedUser.features).toEqual(["create:session"]);
   });
-  test("Login", async () => {});
+  test("Login", async () => {
+    const createSessionResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "registration.flow@curso.dev",
+          password: "RegistrationFlowPassword",
+        }),
+      },
+    );
+
+    expect(createSessionResponse.status).toBe(201);
+
+    const createSessionREsponseBody = await createSessionResponse.json();
+
+    expect(createSessionREsponseBody.user_id).toBe(createUserResponseBody.id);
+  });
   test("Get user information", async () => {});
 });
