e8c8142555c7940ea877b72957fec054e88be18a
Katriel Koga <katriel.koga.2012@hotmail.com>
2026-01-17 18:11:03 -0300

    feat: allow `update:user:others` to update other users

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -89,6 +89,11 @@ function extractUUID(text) {
   return match ? match[0] : null;
 }
 
+async function addFeaturesToUser(userObject, features) {
+  const updatedUser = await user.addFeatures(userObject.id, features);
+  return updatedUser;
+}
+
 const orchestrator = {
   extractUUID,
   waitForAllServices,
@@ -99,6 +104,7 @@ const orchestrator = {
   deleteAllEmails,
   getLastEmail,
   activateUser,
+  addFeaturesToUser,
 };
 
 export default orchestrator;
