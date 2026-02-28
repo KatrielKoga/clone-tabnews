0cc09d127268377b8cc86ac9d4e3b6f287993529
Katriel Koga <katriel.koga.2012@hotmail.com>
2026-02-28 07:57:40 -0300

    refactor: improve error loggin in `email.send()`

--- infra/errors.js
+++ infra/errors.js
@@ -17,10 +17,11 @@ export class InternalServerError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
+  constructor({ cause, message, action, context }) {
     super(message || "Serviço indisponível no momento.", { cause });
     this.name = "ServiceError";
-    this.action = "Verifique se o serviço está disponível.";
+    this.action = action || "Verifique se o serviço está disponível.";
+    this.context = context;
     this.statusCode = 503;
   }
 
@@ -30,6 +31,7 @@ export class ServiceError extends Error {
       message: this.message,
       action: this.action,
       status_code: this.statusCode,
+      context: this.context,
     };
   }
 }
