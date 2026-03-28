b01b23487024b120af5f29cdf164fc0b11f907ba
Katriel Koga <katriel.koga.2012@hotmail.com>
2026-03-28 14:59:17 -0300

    refactor: explicitly return in `/api/v1/status` 'GET' handler

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -48,5 +48,5 @@ async function getHandler(request, response) {
     statusOutput,
   );
 
-  response.status(200).json(secureOutputValues);
+  return response.status(200).json(secureOutputValues);
 }
